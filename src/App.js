a1363e673cf9b72718aec0475190ab9127e0ad5b
devwalex <salamiusmanolawale@gmail.com>
2020-03-30 10:04:26 +0100

    update the content of app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi There. My name is Vue</p>
+        <p>I prefer VueJS to ReactJs</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
